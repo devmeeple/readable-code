3dbc4e36430d9ffc053f09374aa8caf50a7dd65b
미플 <devmeeple@gmail.com>
2024-10-03 15:29:50 +0900

    test(mission): 실패 테스트 3가지
    - 상품을 추가하지 않았을 때
    - 총 가격이 0원일 때
    - 사용자 정보가 없을 때

--- src/test/java/mission/OrderServiceTest.java
+++ src/test/java/mission/OrderServiceTest.java
@@ -29,4 +29,59 @@ class OrderServiceTest {
         // then
         assertThat(sut).isTrue();
     }
+
+    @DisplayName("[실패] 상품을 추가하지 않으면 주문할 수 없다.")
+    @Test
+    void validateOrder_Fail_EmptyItems() {
+        // given
+        Customer customer = Customer.of("유재석", "rebekha_latsonl1fi@trains.iyp");
+        Order order = Order.createOrder(
+                List.of(),
+                12000,
+                customer
+        );
+
+        // when
+        boolean sut = orderService.validateOrder(order);
+
+        // then
+        assertThat(sut).isFalse();
+    }
+
+    @DisplayName("[실패] 총 가격이 0원이면 주문할 수 없다.")
+    @Test
+    void validateOrder_Fail_InvalidTotalPrice() {
+        // given
+        Item item = Item.of("동파육", 12000);
+        Customer customer = Customer.of("유재석", "rebekha_latsonl1fi@trains.iyp");
+        Order order = Order.createOrder(
+                List.of(item),
+                0,
+                customer
+        );
+
+        // when
+        boolean sut = orderService.validateOrder(order);
+
+        // then
+        assertThat(sut).isFalse();
+    }
+
+    @DisplayName("[실패] 사용자 정보가 없으면 주문할 수 없다.")
+    @Test
+    void validateOrder_Fail_NoCustomer() {
+        // given
+        Item item = Item.of("동파육", 12000);
+        Order order = Order.createOrder(
+                List.of(item),
+                12000,
+                null
+        );
+
+        // when
+        boolean sut = orderService.validateOrder(order);
+
+        // then
+        assertThat(sut).isFalse();
+    }
 }

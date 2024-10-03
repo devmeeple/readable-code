75392a504c4ac6007a857d80cecfd275bf5efdc0
미플 <devmeeple@gmail.com>
2024-10-03 15:38:45 +0900

    refactor(mission): Early return 적용

--- src/main/java/mission/OrderService.java
+++ src/main/java/mission/OrderService.java
@@ -9,19 +9,18 @@ public class OrderService {
         if (order.getItems().size() == 0) {
             log.info("주문 항목이 없습니다.");
             return false;
-        } else {
-            if (order.getTotalPrice() > 0) {
-                if (!order.hasCustomerInfo()) {
-                    log.info("사용자 정보가 없습니다.");
-                    return false;
-                } else {
-                    return true;
-                }
-            } else if (!(order.getTotalPrice() > 0)) {
-                log.info("올바르지 않은 총 가격입니다.");
-                return false;
-            }
         }
+
+        if (!(order.getTotalPrice() > 0)) {
+            log.info("올바르지 않은 총 가격입니다.");
+            return false;
+        }
+
+        if (!order.hasCustomerInfo()) {
+            log.info("사용자 정보가 없습니다.");
+            return false;
+        }
+
         return true;
     }
 }
